61e4d93faba817f0986fd4f34557236e07fd7fae
xujian1971 <150315383+xujian1971@users.noreply.github.com>
2025-03-04 20:51:02 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -25,10 +25,15 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES bash"
+PACKAGES="$PACKAGES yq"
+PACKAGES="$PACKAGES jq"
+PACKAGES="$PACKAGES kmod-inet-diag"
+PACKAGES="$PACKAGES kmod-xdp-sockets-diag"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 # 服务——FileBrowser 用户名admin 密码admin
-PACKAGES="$PACKAGES luci-i18n-filebrowser-go-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-daed-zh-cn"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 #24.10
